896d21e9d250de6d4411b7b2fcd25ccfbb5f7060
Arjun Akkiraju <20arjuna@students.harker.org>
2020-10-20 20:19:11 -0400

    json file generation is a go

--- entity-link.py
+++ entity-link.py
@@ -1,4 +1,5 @@
 import tagme
+import json
 
 tagme.GCUBE_TOKEN = "a3cfadd0-cd8b-40a6-8cf2-db83d77692a0-843339462"
 
@@ -8,14 +9,25 @@ questionList = content.split("\n")
 q.close()
 
 jsonData = {}
-jsonData['entity'] = []
-
+jsonData['entities'] = {}
+i = 1
 for question in questionList:
+    #jsonData['entity']['question ' + str(i)] = []
     print("reading: " + question[:10] + " ... ")
     annotation = tagme.annotate(question)
     for ann in annotation.get_annotations(0.1):
-        jsonData['entity'].append(ann)
+        #print(ann.__str__())
+        #print(str(ann))
+        #jsonData['entity']['question ' + str(i)].append(ann.__str__())
+        jsonData['entities'].append(ann.__str__())
+        json_object = json.dumps(jsonData, indent = 4)
+        with open('ATHENA_1.json', 'w') as outfile:
+            outfile.write(json_object)
+        
+    i+=1
     
-    with open('ATHENA_1.json', 'w') as outfile:
-        json.dump(jsonData, outfile)
+    # print(type(jsonData))
+    #print((jsonData))
+
+    
 
