542896c3a8742a347543c1f3cf59fc5cc927e338
Arjun Akkiraju <20arjuna@students.harker.org>
2020-11-21 18:25:43 -0800

    passing data

--- backend/flask_main.py
+++ backend/flask_main.py
@@ -1,9 +1,18 @@
-from flask import Flask,render_template, Response, request, redirect, url_for, send_file
+from flask import Flask, render_template, request,
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return render_template('pastIndex.html')
+
+@app.route('/link', methods=['GET','POST'])
+def main():
+    question = request.form.get("textInput", 0)
+    reverseTxt = text[::-1]
+    return render_template('output.html',
+                            output=request.args.get("output", reverseTxt),
+                            question=request.args.get("question", text))
+
 
 if __name__ == '__main__':
     app.run()
