99f99d50ef31a5fd386e1f6081330adeb589b9a7
Arjun Akkiraju <20arjuna@students.harker.org>
2020-11-23 13:13:27 -0800

    prop extraction is a go

--- backend/wikidata_test.py
+++ backend/wikidata_test.py
@@ -18,10 +18,28 @@ def get_id(query):
     id = json_data["search"][0]["id"]
     return id
 
+def get_props(id):
+    client = Client()
+    entity = client.get(id, load=True)
+
+    occ_prop = str(entity[client.get("P106")])
+    occupation_id = occ_prop[occ_prop.find(' ')+1 : occ_prop.find('>')]
+
+    nat_prop = str(entity[client.get("P27")])
+    nationality_id = nat_prop[nat_prop.find(' ')+1 : nat_prop.find('>')]
+
+    b_prop = str(entity[client.get("P569")])
+    birthday = b_prop[b_prop.find(' ')+1 : b_prop.find('>')]
+
+    props = [occupation_id, nationality_id, birthday_id]
+
+    return props
 
 entity = "alan turing"
-get_id(entity)
+id = get_id(entity)
 
+props = get_props(id)
+print(props)
 #
 # def get_id(entity):
 #
